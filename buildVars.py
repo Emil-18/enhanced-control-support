8fb0c3d483683e5dbaf5971b27372744152826e5
Emil-18 <emilhesmyr@icloud.com>
2023-12-29 21:15:15 +0100

    Updated addon version to 1.0.1

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Adds support for more controls in NVDA."""),
 	# version
-	"addon_version": "1.0",
+	"addon_version": "1.0.1",
 	# Author(s)
 	"addon_author": "Emil-18 <emilhesmyr@icloud.com",
 	# URL for the add-on documentation support
